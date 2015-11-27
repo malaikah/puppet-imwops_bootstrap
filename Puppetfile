15d0a1a8da1c62d0c06965209f6646bd20014acc
BenPriestman <ben.priestman@immediate.co.uk>
2015-11-27 23:56:47 +0000

    Adding path for vbox. Attempting to set machinefolder.

--- Puppetfile
+++ Puppetfile
@@ -5,3 +5,4 @@ forge "https://forgeapi.puppetlabs.com"
 
 mod "puppetlabs-stdlib"
 mod "chocolatey-chocolatey"
+mod "badgerious-windows_env"
--- dist/imwops_bootstrap/manifests/virtualbox.pp
+++ dist/imwops_bootstrap/manifests/virtualbox.pp
@@ -3,6 +3,16 @@ class imwops_bootstrap::virtualbox {
     package {'virtualbox':
         provider    => chocolatey,
         ensure      => present,
+    } ~> windows_env {'PATH':
+        ensure      => present,
+        value       => 'C:\Program Files\Oracle\VirtualBox',
+    }
+    
+    exec {'vbox_machinefolder':
+        path            => $path,
+        command         => "VBoxManage setproperty machinefolder ${FACTER_imwops_workspace}\\vbox_vms",
+        subscribe       => Package['virtualbox'],
+        refreshonly     => true,
     }
 
 }
